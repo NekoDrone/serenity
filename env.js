1fb90420fb5d3cf0786480a5c4a85d131ac40e07
Sylfr <sylfr.serenity.tan@gmail.com>
2023-03-16 18:13:01 +0800

    Updated .gitignore, added instructions on env.js

--- env.js
+++ env.js
@@ -1,3 +1,8 @@
+//when setting up, rename this file to 'local.js', and insert your keys accordingly.
+
+/*your gcal_id is the ID of the calendar you would like this bot to manage.
+If you are targeting your primary calendar, this would be your gmail address.*/
+
 export const bot_id = "insert bot id here";
 export const gapi_key = "insert gapi key here";
-export const gcal_id = "insert gcal id here"
+export const gcal_id = "insert gcal id here";
