755187bf3db833f9ec38723518d677da15f3671e
Sylfr <sylfr.serenity.tan@gmail.com>
2023-06-02 08:50:00 +0800

    Updated dependencies. Reworked env variables.

--- src/env.ts
+++
@@ -1,8 +0,0 @@
-//when setting up, rename this file to 'creds.js', and insert your keys accordingly. KEEP 'creds.js' A SECRET
-
-/*your gcal_id is the ID of the calendar you would like this bot to access.
-If you are targeting your primary calendar, this would be your gmail address.*/
-
-export const bot_id = "insert bot id here";
-export const gapi_key = "insert gapi key here";
-export const gcal_id = "insert gcal id here";
--- src/urls.ts
+++ src/urls.ts
@@ -1,5 +1,26 @@
-import { bot_id } from "./creds";
-import { gcal_id } from "./creds";
+const dotenv = require('dotenv');
+const keys = dotenv.config();
 
-export const baseTeleBotURL = "https://api.telegram.org/bot" + bot_id;
-export const baseCalURL = "https://www.googleapis.com/calendar/v3/calendars/" + gcal_id;
+if(keys.error){
+    throw keys.error;
+}
+console.log("Successfully parsed the following keys:")
+console.log(keys.parsed);
+
+const baseTeleBotURL = "https://api.telegram.org/bot" + process.env.BOT_ID;
+const baseCalURL = "https://www.googleapis.com/calendar/v3/calendars/" + process.env.GCAL_ID;
+
+if(baseTeleBotURL == "https://api.telegram.org/botinsert bot id here"){
+    console.warn("Telegram Bot ID not set. Please set it in '.env'.");
+}
+
+if(baseCalURL == "https://www.googleapis.com/calendar/v3/calendars/insert gcal id here"){
+    console.warn("Google Calendar not set. Please set it in '.env'.");
+}
+
+if(process.env.GAPI_KEY == "insert gapi key here"){
+    console. warn("Google API Key not set.  PLease set it in '.env'.")
+}
+
+console.log("Loaded base Telegram Bot URL at: " + baseTeleBotURL);
+console.log("Loaded base Google Calendar URL at: " + baseCalURL);
